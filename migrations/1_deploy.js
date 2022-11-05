b22c4c12c594130efa207177964b6268ff75e9b8
Arogundade Ibrahim <devarogundade@gmail.com>
2022-11-05 15:30:52 +0100

    added stable coins

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,9 +1,20 @@
 /* contracts */
 const Dash = artifacts.require("Dash");
 const DashToken = artifacts.require("DashToken");
+const FUSD = artifacts.require("FUSD");
+const USDT = artifacts.require("USDT");
+const USDC = artifacts.require("USDC");
 
 /* fantom testnet */
 module.exports = async function(deployer) {
+    // deploy dash token
     await deployer.deploy(DashToken, "Dash Token", "DASH")
+
+    // deploy dash smart contract
     await deployer.deploy(Dash, DashToken.address)
+
+    // deploy stable coins
+    await deployer.deploy(FUSD)
+    await deployer.deploy(USDT)
+    await deployer.deploy(USDC)
 };
