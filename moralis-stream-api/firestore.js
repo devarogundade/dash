7e1effb497f752e7f3da2804f49752d41b813f13
Arogundade Ibrahim <devarogundade@gmail.com>
2022-11-06 17:59:46 +0100

    fix firestore

--- moralis-stream-api/firestore.js
+++ moralis-stream-api/firestore.js
@@ -6,7 +6,7 @@ initializeApp({ credential: cert(serviceAccount) });
 
 module.exports = {
     db: getFirestore(),
-    write: async function(collection, document, data, _merge = false) {
+    write: async function(collection, document, data, _merge) {
         try {
             const reference = this.db.collection(collection).doc(document);
             await reference.set(data, { merge: _merge })
